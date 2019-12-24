f8f455563983ca3a35791f065e150be41f8317a0
Daniel Lopes <daniel.braz@am4.com.br>
2019-12-24 11:14:23 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function src() {
+    const a = 1;
+    return <View style={{ flex: 1 }} />;
+}
